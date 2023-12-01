38001a677c52540fa357816c4e167626bc77c05a
khushi <122344362+khushidurge@users.noreply.github.com>
2023-12-01 20:25:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+}
+
+/*initialize swiper*/
+var swiper = new Swiper(".home-slider",  {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop : true,
+});
+
